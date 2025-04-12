2cec6aeb6b13ff1ed85dfd146324be9b8987d328
AnujRawat1 <rawatanuj058@gmail.com>
2025-04-12 14:08:56 +0530

    domain

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-export const baseURL = "http://localhost:8080";
+export const baseURL =
+  window.location.protocol === 'https:'
+    ? 'https://linkit-ten.vercel.app' 
+    : 'http://localhost:8080';  
+
 
 export const httpClient = axios.create({
     baseURL: baseURL,
