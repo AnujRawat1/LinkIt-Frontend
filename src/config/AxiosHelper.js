52aad87f9909279e1f1057c4309f625591a944a1
AnujRawat1 <rawatanuj058gmail.com>
2025-08-09 09:02:24 +0530

    Added Production backend Webserver API

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const baseURL_PROD = 'https://linkit-latest.onrender.com'; 
+export const baseURL_PROD = 'https://linkit-backend.onrender.com'; 
 export const baseURL_DEV = ' http://localhost:8080';  
 
 export const httpClient = axios.create({
-    baseURL: baseURL_DEV,
+    baseURL: baseURL_PROD,
 });
